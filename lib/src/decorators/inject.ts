c95d4c286341d717979a481d1d3a9683821ea6d1
lahcene <benzara.tahar@gmail.com>
2024-11-13 14:16:24 +0100

    feat: add builtin @Inject tokens, with possibility to use custom token

--- lib/src/decorators/inject.ts
+++ lib/src/decorators/inject.ts
@@ -1,7 +1,96 @@
 
-// to provide typesafety accross the app, add any new service used here
-// TODO: make this generic
-export type Injectable = "$scope" | "TodoService";
+
+
+// Core services and providers
+export type AngularJsInjectTokens =
+    // Core services
+    '$anchorScroll' |
+    '$animate' |
+    '$cacheFactory' |
+    '$compile' |
+    '$controller' |
+    '$document' |
+    '$exceptionHandler' |
+    '$filter' |
+    '$http' |
+    '$httpBackend' |
+    '$httpParamSerializer' |
+    '$httpParamSerializerJQLike' |
+    '$interpolate' |
+    '$interval' |
+    '$locale' |
+    '$location' |
+    '$log' |
+    '$parse' |
+    '$q' |
+    '$rootElement' |
+    '$rootScope' |
+    '$scope' |
+    '$sce' |
+    '$sceDelegate' |
+    '$templateCache' |
+    '$templateRequest' |
+    '$timeout' |
+    '$window' |
+    '$xhrFactory' |
+
+    // Additional services
+    '$cookies' |
+    '$cookieStore' |
+    '$resource' |
+    '$route' |
+    '$routeParams' |
+    '$routeProvider' |
+    '$sanitize' |
+    '$swipe' |
+
+    // Provider tokens
+    '$animateProvider' |
+    '$compileProvider' |
+    '$controllerProvider' |
+    '$filterProvider' |
+    '$httpProvider' |
+    '$interpolateProvider' |
+    '$locationProvider' |
+    '$logProvider' |
+    '$parseProvider' |
+    '$rootScopeProvider' |
+    '$sceDelegateProvider' |
+    '$sceProvider' |
+    '$templateRequestProvider' |
+
+    // Constants and values
+    '$window' |
+    '$document' |
+
+    // UI-Router specific
+    '$state' |
+    '$stateParams' |
+    '$stateProvider' |
+    '$urlRouter' |
+    '$urlRouterProvider' |
+    '$uiViewScroll' |
+
+    // Form validation
+    '$validator' |
+    '$asyncValidator' |
+
+    // Modal service
+    '$modal' |
+    '$modalInstance' |
+    '$modalStack' |
+
+    // Common module services
+    '$base64' |
+    '$drag' |
+    '$drop' |
+    '$position' |
+    '$scrollspy' |
+    '$tooltip' |
+    '$transition';
+
+// Create a generic type parameter for user-defined service types
+export type Injectable<T extends AngularJsInjectTokens = never> = AngularJsInjectTokens | T  |  (string & {});
 export function Inject(...dependencies: readonly Injectable[]) : ClassDecorator {
 
     return function (target) : any {
