978d2316f9607cbbb2ab9c906ba2982aca214a7e
lahcene <benzara.tahar@gmail.com>
2024-11-13 04:09:56 +0100

    wip

--- lib/jest.config.ts
+++ lib/jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleFileExtensions: ['ts', 'js'],
   testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
@@ -10,10 +11,6 @@ const config: Config = {
     '^.+\\.tsx?$': ['ts-jest', {
       tsconfig: 'tsconfig.json'
     }]
-  },
-  moduleNameMapper: {
-    '^angular$': '<rootDir>/node_modules/angular/angular.js',
-    '^angular-mocks$': '<rootDir>/node_modules/angular-mocks/angular-mocks.js'
   }
 };
 
